d0e1368f376321405d63c21aad0f63a4aec9ab69
malki.abdoo <107503586+malkiAbdoo@users.noreply.github.com>
2022-08-30 12:31:44 +0100

    Modifing the file description

--- ascii_video_player/asciivp.py
+++ ascii_video_player/asciivp.py
@@ -10,7 +10,7 @@ def main():
 
     # get the arguments
     PARSER = argparse.ArgumentParser(prog="asciivp", description=desc, epilog=eplg)
-    PARSER.add_argument('file', help="the file path of a video or a GIF.")
+    PARSER.add_argument('file', help="the file path or URL of a video or a GIF.")
     PARSER.add_argument('-r', '--replay', help="Replay the video automatically when the video ends.", action="store_true")
     PARSER.add_argument('-s', '--size', help="Set a size to the video.", type=str)
     PARSER.add_argument('-c', '--chars',  default=" .'~;icok0XN",type=str,
