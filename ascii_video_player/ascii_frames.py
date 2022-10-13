190e069ae9505150bba358e4a71dba410d1cbaff
malkiAbdoo <abdo.malkiep@gmail.com>
2022-10-13 21:17:34 +0100

    Using ANSI colors

--- ascii_video_player/ascii_frames.py
+++ ascii_video_player/ascii_frames.py
@@ -3,16 +3,23 @@ import sys
 import cv2
 
 
-def image2ascii(frame, size, chars):
+def colorize(char, rgb):
+    b, g, r = rgb
+    return f"\x1b[38;2;{r};{g};{b}m{char}\x1b[0m"
+
+def image2ascii(frame, size, chars, colors):
+    frame = rescale_frame(frame, size)
+
     # convert to grayscale image
-    frame = cv2.cvtColor(rescale_frame(frame, size), cv2.COLOR_BGR2GRAY)
+    f = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # replace each pixel with a character from chars
-    def mappingPixels(pxls):
-        lim = 255 // (len(chars) - 1)
-        return ''.join(list(map(lambda p: chars[p//lim], pxls)))
+    area = 255 // (len(chars) - 1)
+    def mapping_pixels(pixels=None):
+        pixels = zip(pixels[0], pixels[1])
+        return ''.join(list(map(lambda p: chars[p[0]//area] if not colors else colorize(chars[p[0]//area], p[1]), pixels)))
 
-    ascii_img = list(map(lambda pxls: mappingPixels(pxls), frame))
+    ascii_img = list(map(mapping_pixels, zip(f, frame)))
     ascii_img = '\n'.join(ascii_img)
 
     return ascii_img
@@ -28,7 +35,7 @@ def rescale_frame(frame, size):
         # get the terminal height
         lines = os.get_terminal_size()[1]
 
-        height = lines - 2
+        height = lines - 1
         width = int(frame.shape[1] * height * 2 / frame.shape[0])
     dim = (width, height)
 
